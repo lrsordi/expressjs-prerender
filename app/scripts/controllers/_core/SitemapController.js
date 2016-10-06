61a0401129c9a8cb093a7ff224951a7f955455f1
Luiz Sordi <lrsordi@gmail.com>
2016-10-06 01:58:00 -0300

    correction in SitemapController

--- app/scripts/controllers/_core/SitemapController.js
+++ app/scripts/controllers/_core/SitemapController.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var AbstractController = require('../_base/AbstractController');
 
 var SitemapController = AbstractController.extend({
 
