76e921d3f2302553d465fde7bb95aae6369f7c06
Luiz Sordi <luiz.sordi@huia.com.br>
2016-10-15 20:48:30 -0300

    some tests with svg and text

--- app/scripts/views/sections/home/components/CarouselComponent.js
+++ app/scripts/views/sections/home/components/CarouselComponent.js
@@ -1,5 +1,8 @@
 var Backbone = require('backbone');
 var BaseComponent = require('../../../_base/BaseComponentView');
+var opentype = require("opentype.js");
+require('gsap');
+require('../../../../vendors/DrawSVGPlugin');
 
 
 
@@ -13,9 +16,29 @@ var CarouselComponent = BaseComponent.extend({
 
   build : function(){
     $domEl = $("#" + this.identifier);
-    $domEl.find("span").click(function(evt){
-      alert("aeeaeae");
-    });
+    /*
+    document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    document.createElementNS("http://www.w3.org/2000/svg", "path");
+    document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+    opentype.load('public/fonts/intro-regular.otf', function(err, font) {
+      var path = font.getPath('teste aqui', 0, 150, 150);
+      var el = path.toSVG(2);
+      var dStr = el.replace("<path d=\"","").replace("\"/>","");
+
+      $domEl.find("svg g g").append(el);
+      el = $domEl.find("svg g g")[0];
+      newpath = document.createElementNS('http://www.w3.org/2000/svg',"path");
+      newpath.setAttributeNS(null, "d",dStr);
+      console.log(newpath);
+      window.el = el;
+      el.appendChild(newpath);
+
+      TweenMax.set($domEl.find("path"), {drawSVG:"0%", fillOpacity:0});
+      TweenMax.to($domEl.find("path"),5,{drawSVG:"100%", delay:1, ease : Linear.easeInOut});
+      TweenMax.to($domEl.find("path"), 1, {fillOpacity:0.3, delay:1, ease : Linear.easeNone, overwrite:false});
+      // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
+  });*/
   },
 });
 
--- app/templates/home_carousel.tpl
+++ app/templates/home_carousel.tpl
@@ -1,4 +1,18 @@
 <div id="<%- identifier %>">
   <a href="/sobre-o-projeto" class="history-link">teste btn</a>
-  <span>teste do contiudo </span>
+  <span class="svg-txt">teste do contiudo</span>
+
+
+  <svg version="1.1" id="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 1498.11 409" style="enable-background:new 0 0 1498.11 409;" xml:space="preserve">
+    <style type="text/css">
+    	path {fill:red;fill-opacity:0;stroke:red;stroke-width:4;stroke-linecap:round;stroke-linejoin:round;}
+    </style>
+
+    <g id="DrawSVG">
+    	<g>
+    	</g>
+    </g>
+</svg>
+
 </div>
